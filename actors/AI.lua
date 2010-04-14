d52fe3af41f6cb73cf6dbf733f279566e3a229df
kikito <kikito@gmail.com>
2010-04-14 08:04:19 +0000

    moved rotation logic to AI.lua

--- actors/AI.lua
+++ actors/AI.lua
@@ -1,5 +1,16 @@
 AI = class('AI', passion.Actor)
 
+local twoPi = 2.0*math.pi
+
+function _sign(x)
+  return x>0 and 1 or x<0 and -1 or 0
+end
+
+local _normalizeAngle = function(angle)
+  angle = angle % twoPi
+  return (angle < 0 and (angle + twoPi) or angle)
+end
+
 function AI:initialize()
   super.initialize(self)
 end
@@ -10,4 +21,28 @@ end
 
 function AI:getVehicle(vehicle)
   return self.vehicle
+end
+
+function AI:orientateTowards(tx,ty)
+
+  local vehicle = self:getVehicle()
+  local x, y = vehicle:getPosition()
+  local angle = vehicle:getAngle()
+  local maxTorque = vehicle:getRotation()
+  local inertia = vehicle:getInertia()
+  local w = vehicle:getAngularVelocity()
+
+  local targetAngle = math.atan2(ty-y,tx-x)
+  -- Distance I have to cover
+  local differenceAngle = _normalizeAngle(targetAngle - angle)
+  -- Distance it will take me to stop
+  local brakingAngle = _normalizeAngle(_sign(w)*2.0*w*w*inertia/maxTorque)
+
+  -- two of these 3 conditions must be true
+  local a,b,c = differenceAngle > math.pi, brakingAngle > differenceAngle, w > 0
+  if( (a and b) or (a and c) or (b and c) ) then
+    return 'counterclockwise'
+  end
+
+  return 'clockwise'
 end
--- actors/PlayerAI.lua
+++ actors/PlayerAI.lua
@@ -3,15 +3,13 @@ require('actors/AI')
 PlayerAI = class('PlayerAI', AI)
 PlayerAI:includes(Beholder)
 
-local twoPi = 2.0*math.pi
-
 function PlayerAI:initialize()
   super.initialize(self)
   self._thrust = false
   self._strafeLeft = false
   self._strafeRight = false
   self._fire = false
-  
+
   self:observe('keypressed_w', function(self) self._thrust = true end)
   self:observe('keypressed_a', function(self) self._strafeLeft = true end)
   self:observe('keypressed_d', function(self) self._strafeRight = true end)
@@ -32,40 +30,8 @@ function PlayerAI:getStrafeDirection()
   return nil
 end
 
-function _sign(x)
-  return x>0 and 1 or x<0 and -1 or 0
-end
-
-local _normalizeAngle = function(angle)
-  angle = angle % twoPi
-  return (angle < 0 and (angle + twoPi) or angle)
-end
-
 function PlayerAI:getRotationDirection()
-
-  local ox, oy = love.mouse.getPosition()
-
-  local vehicle = self:getVehicle()
-  local x, y = vehicle:getPosition()
-  local angle = vehicle:getAngle()
-  local maxTorque = vehicle:getRotation()
-  local inertia = vehicle:getInertia()
-  local w = vehicle:getAngularVelocity()
-
-  local targetAngle = math.atan2(oy-y,ox-x)
-  -- difference between my angle and my target angle
-  local differenceAngle = _normalizeAngle(targetAngle - angle)
-  -- the distance i would move before stopping, if I started stopping just now, with my current w
-  local brakingAngle = _normalizeAngle(_sign(w)*2.0*w*w*inertia/maxTorque)
-
-  local torque = maxTorque
-  local a,b,c = differenceAngle > math.pi, brakingAngle > differenceAngle, w > 0
-  -- two of these 3 conditions must be true
-  if( (a and b) or (a and c) or (b and c) ) then
-    torque = -torque
-  end
-
-  return torque > 0 and 'clockwise' or 'counterclockwise'
+  return self:orientateTowards(love.mouse.getPosition())
 end
 
 function PlayerAI:getWeaponsFired()
